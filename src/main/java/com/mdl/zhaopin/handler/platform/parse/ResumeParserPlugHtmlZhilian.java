3ca272b1b4db7dfb77bf7af30f11bcd439d9e80b
谢昆 <xiekun@sunlands.com>
2020-04-20 18:07:16 +0800

    nil

--- src/main/java/com/mdl/zhaopin/handler/platform/parse/ResumeParserPlugHtmlZhilian.java
+++ src/main/java/com/mdl/zhaopin/handler/platform/parse/ResumeParserPlugHtmlZhilian.java
@@ -169,50 +169,35 @@ public class ResumeParserPlugHtmlZhilian extends AbstractResumeParser implements
 //    }
 
 
-    /**
-     * 在校情况
-     *
-     * @param next
-     */
-    private void getUniversityBaseInfo(Element next) {
-        //#resumeDetail > div:nth-child(10) > div.resume-content__section-body.resume-content--timeline.is-mt-10.is-mb-20 > dl > dd
-        Elements select = next.select("div.resume-content__section-body.resume-content--timeline.is-mt-10.is-mb-20");
-        for (Element element : select) {
-            String 在校情况 = element.text();
-            System.out.println("\n在校情况:" + 在校情况);
-        }
-    }
+//    /**
+//     * 语言能力
+//     *
+//     * @param languageBaseInfoHtml
+//     */
+//    private void getLanguageBaseInfo(String languageBaseInfoHtml) {
+//        Document document = parse2Html(languageBaseInfoHtml);
+//        Elements select = document.select("ul > li");
+//        for (Element element : select) {
+//            String 语言能力 = element.text();
+//            System.out.println("\n语言能力:" + 语言能力);
+//        }
+//    }
 
 
-    /**
-     * 语言能力
-     *
-     * @param next
-     */
-    private void getLanguageBaseInfo(Element next) {
-        //#resumeDetail > div:nth-child(12) > ul > li
-        Elements select = next.select("ul > li");
-        for (Element element : select) {
-            String 语言能力 = element.text();
-            System.out.println("\n语言能力:" + 语言能力);
-        }
-    }
-
-
-    /**
-     * 证书名字及时间
-     *
-     * @param next
-     */
-    private void getCertificateBaseInfo(Element next) {
-        //#resumeDetail > div:nth-child(9) > ul
-        Elements ul = next.select("ul > li");
-        for (Element element : ul) {
-            //#resumeDetail > div:nth-child(9) > ul > li:nth-child(1)
-            String 证书名字及时间 = element.text();
-            System.out.println("\n证书名字及时间：" + 证书名字及时间);
-        }
-    }
+//    /**
+//     * 证书名字及时间
+//     *
+//     * @param certificateBaseInfoHtml
+//     */
+//    private void getCertificateBaseInfo(String certificateBaseInfoHtml) {
+//        Document document = parse2Html(certificateBaseInfoHtml);
+//        Elements ul = document.select("ul > li");
+//        for (Element element : ul) {
+//            //#resumeDetail > div:nth-child(9) > ul > li:nth-child(1)
+//            String 证书名字及时间 = element.text();
+//            System.out.println("\n证书名字及时间：" + 证书名字及时间);
+//        }
+//    }
 
 
     /**
@@ -237,31 +222,27 @@ public class ResumeParserPlugHtmlZhilian extends AbstractResumeParser implements
      * @param selfEvalBaseInfoHtml
      */
     private void getSelfEvalBaseInfo(String selfEvalBaseInfoHtml) {
-
         Document document = parse2Html(selfEvalBaseInfoHtml);
         String 自我评价 = document.select("div").text().trim();
         System.out.println("\n自我评价:" + 自我评价);
     }
 
-    /**
-     * 专业技能
-     *
-     * @param next
-     */
-    private void getSkillBaseInfo(Element next) {
-        //#resumeDetail > div:nth-child(5) > ul
-        Elements select = next.select("ul > li");
-        for (Element element : select) {
-            //#resumeDetail > div:nth-child(5) > ul > li:nth-child(1) > p.is-weight-bold
-            String 技能名称 = element.select("p.is-weight-bold").text();
-            //#resumeDetail > div:nth-child(5) > ul > li:nth-child(1) > p.resume-content__labels--sub > span:nth-child(1)
-            String 技能掌握程度 = element.select("p.resume-content__labels--sub > span:nth-child(1)").text();
-            //#resumeDetail > div:nth-child(5) > ul > li:nth-child(1) > p.resume-content__labels--sub > span:nth-child(2)
-            String 技能掌握时长 = element.select("p.resume-content__labels--sub > span:nth-child(2)").text();
 
-            System.out.println("\n技能名称:" + 技能名称 + "\n技能掌握程度:" + 技能掌握程度 + "\n技能掌握时长:" + 技能掌握时长);
-        }
-    }
+//    /**
+//     * 专业技能
+//     *
+//     * @param skillBaseInfoHtml
+//     */
+//    private void getSkillBaseInfo(String skillBaseInfoHtml) {
+//        Document document = parse2Html(skillBaseInfoHtml);
+//        Elements select = document.select("ul > li");
+//        for (Element element : select) {
+//            String 技能名称 = element.select("p.is-weight-bold").text();
+//            String 技能掌握程度 = element.select("p.resume-content__labels--sub > span:nth-child(1)").text();
+//            String 技能掌握时长 = element.select("p.resume-content__labels--sub > span:nth-child(2)").text();
+//            System.out.println("\n技能名称:" + 技能名称 + "\n技能掌握程度:" + 技能掌握程度 + "\n技能掌握时长:" + 技能掌握时长);
+//        }
+//    }
 
 
     /**
@@ -288,9 +269,7 @@ public class ResumeParserPlugHtmlZhilian extends AbstractResumeParser implements
      * @param workExpBaseInfoHtml
      */
     private void getWorkExpBaseInfo(String workExpBaseInfoHtml) {
-
         Document document = parse2Html(workExpBaseInfoHtml);
-
         Elements select = document.select("body > ul > li");
         for (Element element : select) {
             String 开始时间 = element.select("[data-bind = text: data.workDate.beginDate]").text();
@@ -304,23 +283,6 @@ public class ResumeParserPlugHtmlZhilian extends AbstractResumeParser implements
             System.out.println("\n开始时间:" + 开始时间 + "\n结束时间:" + 结束时间 + "\n公司名字:" + 公司名字
                     + "\n就职时长:" + 就职时长 + "\n岗位:" + 岗位 + "\n薪资:" + 薪资 + "\n工作描述:" + 工作描述);
         }
-
-//        //#resumeDetail > div:nth-child(4) > ul > li:nth-child(1) > p.timeline__header
-//        Elements select = next.select("ul > li");
-//        for (Element element : select) {
-//            //#resumeDetail > div:nth-child(4) > ul > li:nth-child(1) > p.timeline__header > span.timeline__time--wrapper
-//            String 起止时间 = element.select("p > span.timeline__time--wrapper").text();
-//            //#resumeDetail > div:nth-child(4) > ul > li:nth-child(1) > p.timeline__header > span:nth-child(2)
-//            String 公司 = element.select("p > span:nth-child(2)").text();
-//            //#resumeDetail > div:nth-child(4) > ul > li:nth-child(2) > p.timeline__header > span.timeline__time-range
-//            String 就职时长 = element.select("p > span:nth-child(3)").text();
-//            //#resumeDetail > div:nth-child(4) > ul > li:nth-child(2) > p.resume-content__skill-tags
-//            String 技能标签 = element.select("p.resume-content__skill-tags").text();
-//            //#resumeDetail > div:nth-child(4) > ul > li:nth-child(2) > dl > dd
-//            String 工作描述 = element.select("dl > dd").text();
-//
-//            System.out.println("\n起止时间:" + 起止时间 + "\n公司:" + 公司 + "\n就职时长:" + 就职时长 + "\n技能标签:" + 技能标签 + "\n工作描述:" + 工作描述);
-//        }
     }
 
 
@@ -347,7 +309,6 @@ public class ResumeParserPlugHtmlZhilian extends AbstractResumeParser implements
      * @param JobHuntBaseInfoHtml
      */
     private void getJobHuntBaseInfo(String JobHuntBaseInfoHtml) {
-
         Element resumeDetail = parse2Html(JobHuntBaseInfoHtml);
 
         List<String> keyList = resumeDetail.select("[data-bind = foreach: { data: intents, as: 'intent' }]")
@@ -355,8 +316,6 @@ public class ResumeParserPlugHtmlZhilian extends AbstractResumeParser implements
 
         List<String> valList = resumeDetail.select("[data-bind = foreach: { data: intents, as: 'intent' }]")
                 .select("dd").stream().map(Element::text).collect(Collectors.toList());
-
-        System.out.println(keyList.size() + "---" + valList.size());
 
         Map<String, String> resMap = new HashMap<>();
         for (int i = 0; i < keyList.size(); i++) {
@@ -386,9 +345,7 @@ public class ResumeParserPlugHtmlZhilian extends AbstractResumeParser implements
      * @param seekerBaseInfoHtml
      */
     private void getSeekerBaseInfo(String seekerBaseInfoHtml) {
-
         Element seekerBaseInfo = parse2Html(seekerBaseInfoHtml);
-
         /** 名字 **/
         String 名字 = seekerBaseInfo.select("[data-bind = textQ: candidateName]").text();
         /** 性别 **/
@@ -476,6 +433,13 @@ public class ResumeParserPlugHtmlZhilian extends AbstractResumeParser implements
 //        String html = FileUtils.readFileToString(file, "UTF-8");
 //        ResumeParserPlugHtmlZhilian resumeParser = new ResumeParserPlugHtmlZhilian();
 //        resumeParser.getSelfEvalBaseInfo(html);
+
+
+//        String filePath = "/Users/admin/Desktop/简历解析/智联招聘-插件-任女士/培训经历.html";
+//        File file = new File(filePath);
+//        String html = FileUtils.readFileToString(file, "UTF-8");
+//        ResumeParserPlugHtmlZhilian resumeParser = new ResumeParserPlugHtmlZhilian();
+//        resumeParser.getTrainBaseInfo(html);
 
 
         String filePath = "/Users/admin/Desktop/简历解析/智联招聘-插件-任女士/培训经历.html";
